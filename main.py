4f8ad181cb684912de22326dd4a2a15e3308ced9
Frillion <fridriksoebech@gmail.com>
2024-08-20 22:02:02 +0000

    rendered triangle/player on the screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -7,13 +8,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    delta_time = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        delta_time = time.tick(60)/1000
 
 if __name__ == "__main__":
     main()
