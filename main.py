7c6959a5036c54b5176af15d4679cc2bb2d715be
Frillion <fridriksoebech@gmail.com>
2024-08-20 23:19:42 +0000

    you can now shoot and loose the game to asteroids

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+from shot import Shot
+from asteroidfield import *
+from asteroid import *
 from player import Player
 from constants import *
 
@@ -12,15 +15,35 @@ def main():
     delta_time = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (updatable, drawable, asteroids) 
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, drawable, updatable)
+
     player = Player(x, y)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(delta_time)
         screen.fill((0,0,0,0))
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(delta_time)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        for obj in asteroids:
+            if obj.is_colliding(player):
+                print("Game over!")
+                return 
+
         pygame.display.flip()
         delta_time = time.tick(60)/1000
 
--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-     
+        self.timer = 0 
+
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -16,11 +18,23 @@ class Player(CircleShape):
         c = self.position - ( forward * self.radius ) + right
         return [a, b, c]
 
+    def shoot(self):
+        if self.timer > 0:
+            shot = Shot(self.position.x, self.position.y)
+            unit = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            shot.velocity = unit
+
+        self.timer = PLAYER_SHOT_COOLDOWN
+
     def rotate(self, delta_time):
         self.rotation += delta_time * PLAYER_TURN_SPEED
 
     def update(self, delta_time):
         keys = pygame.key.get_pressed()
+        self.timer -= delta_time        
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
         if keys[pygame.K_a]:
            self.rotate(-delta_time) 
